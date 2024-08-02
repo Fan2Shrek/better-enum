2297d2d21e5ba4b53dedc4d6050ccb005d899dfd
Pierre Ambroise <pierre27.ambroise@gmail.com>
2024-08-02 11:46:49 +0200

    Update test file

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <?php
 
-use Fan2Shrek\BetterEnum\Cache\FileCache;
-
 require_once __DIR__ . '/vendor/autoload.php';
 
-$enumerator = new Fan2Shrek\BetterEnum\Enumerator(new FileCache(__DIR__ . '/var/cache'));
+// $enumerator = new Fan2Shrek\BetterEnum\Enumerator();
+$enumerator = new Fan2Shrek\BetterEnum\Enumerator(new Fan2Shrek\BetterEnum\Cache\FileCache(__DIR__ . '/var/cache'));
 
 $metadata = new Fan2Shrek\BetterEnum\DynamicEnum\DynamicEnumMetadata('TestEnum', 'int', 'App');
 
@@ -17,8 +16,8 @@ $enumerator->generateEnum($metadata);
 $enumerator->createMultipleValueEnum(new Fan2Shrek\BetterEnum\DynamicEnum\MultipleValueEnum('LinuxPermissionEnum', 'App', ['EXECUTE', 'WRITE', 'READ']));
 
 $entity = new \stdClass();
-$entity->roles = App\LinuxPermissionEnum::getValue([App\LinuxPermissionEnum::READ, App\LinuxPermissionEnum::WRITE]);
-$entity->roles = App\LinuxPermissionEnum::getEnums(5);
+$entity->roles = App\LinuxPermissionEnum::getValue([App\LinuxPermissionEnum::READ, App\LinuxPermissionEnum::EXECUTE]);
+$entity->roles = App\LinuxPermissionEnum::getEnums(7);
 
 foreach($entity->roles as $role) {
     echo $role->name . PHP_EOL;
